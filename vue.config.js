0029a4f52bd0b8e7157012f911d4d94da36048d0
Anne-Sophie <annesophie.faustino@gmail.com>
2023-03-13 10:26:36 +0400

    change name dist folder

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/mon-portfolio/" : "/",
   transpileDependencies: ["vuetify"],
-  outputDir: "docs"
+  outputDir: "dist"
 };
