019aef31086b1aaa682b09b747155faa2608ae5d
Anne-Sophie <annesophie.faustino@gmail.com>
2023-03-13 10:22:08 +0400

    deploy

--- src/router/index.js
+++ src/router/index.js
@@ -67,7 +67,7 @@ const routes = [
     component: MielPei
   },
   {
-    path: "/mon-portfolio",
+    path: "/portfolio",
     name: "MonPortfolio",
     component: MonPortfolio
   },
@@ -80,7 +80,6 @@ const routes = [
 
 const router = new Router({
   mode: "history",
-  base: "/mon-portfolio/",
   routes
 });
 
